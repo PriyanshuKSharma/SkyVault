cb8d352f32e7a6e8f005d966760e774942d18074
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-08-22 13:02:57 +0530

    Hosted SkyVault secure cloud storage

--- app.py
+++ app.py
@@ -220,8 +220,5 @@ def signup():
 
     return render_template('signup.html', title="Sign Up")
 
-app.run(host='0.0.0.0', port=5000)
-
-
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', ssl_context=None)
+    app.run(debug=True, host='0.0.0.0', port=5000)
